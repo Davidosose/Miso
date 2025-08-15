17ad12db6b6f748c58f0295413e432fe0d259347
David Osose <144554752+Davidosose@users.noreply.github.com>
2025-08-15 21:03:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ function toggleBrands(id) {
   document.getElementById(id).classList.toggle('hidden');
 }
 ;
+
+document.getElementById("paymentForm").addEventListener("submit", function(e){
+  e.preventDefault();
+            
+  let name = document.getElementById("name").value;
+  let amount = document.getElementById("amount").value;
+  let details = document.getElementById("details").value;
+
+  let message = `Hello, I want to confirm my payment.%0A%0AName: ${name}%0AAmount Paid: ${amount}%0ADetails: ${details}%0A%0AI will attach my payment proof below.`;
+            
+   // Replace with your WhatsApp number (international format without +)
+    let phone = "2349129378349"; 
+            
+    let url = `https://wa.me/${phone}?text=${message}`;
+    window.open(url, "_blank");
+ });
